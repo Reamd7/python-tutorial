69a1b6a2af22efe4f45e59eea1670962ba6915a1
reamd7 <reamd7@gmail.com>
2025-07-08 01:11:01 +0800

    feat: 挺复杂的这些数据结构里面的操作

--- exercises/chapter05_data_structures.py
+++ exercises/chapter05_data_structures.py
@@ -14,8 +14,7 @@
 
 from collections import deque
 
-
-def list_methods_comprehensive(original_list, item_to_add, item_to_remove):
+def list_methods_comprehensive[T](original_list: list[T], item_to_add: T, item_to_remove: T):
     """
     练习 5.1: 列表方法综合练习
     
@@ -29,17 +28,50 @@ def list_methods_comprehensive(original_list, item_to_add, item_to_remove):
     """
     # TODO: 返回包含以下操作结果的字典：
     # 'append_result': 使用 append() 添加 item_to_add 后的列表
+    append_result = original_list.copy()
+    append_result.append(item_to_add)
     # 'insert_result': 在索引 1 处插入 item_to_add 的结果
+    insert_result = original_list.copy()
+    insert_result.insert(1, item_to_add)
     # 'remove_result': 移除第一个 item_to_remove 的结果（如果存在）
+    remove_result = original_list.copy()
+    remove_result.remove(item_to_remove)
     # 'pop_result': 弹出最后一个元素后的 (剩余列表, 弹出的元素)
+    pop_result = original_list.copy()
+    pop_result.pop()
     # 'index_result': item_to_add 的索引（如果不存在返回 -1）
+    index_result = original_list.copy()
+    index_result.index(item_to_add)
     # 'count_result': item_to_add 在列表中的出现次数
+    count_result = original_list.copy()
+    count_result.count(item_to_add)
     # 'sort_result': 排序后的列表（如果可排序）
+    sort_result = original_list.copy()
+    # sort_result.sort()
     # 'reverse_result': 反转后的列表
-    pass
+    reverse_result = original_list.copy()
+    reverse_result.reverse()
+    return {
+        'append_result': append_result,
+        'insert_result': insert_result,
+        'remove_result': remove_result,
+        'pop_result': pop_result,
+        'index_result': index_result,
+        'count_result': count_result,
+        # 'sort_result': sort_result,
+        'reverse_result': reverse_result,
+    }
 
 
-def stack_operations(operations):
+from typing import TypeAlias, TypedDict
+from typing import Union, Literal, Any, TypeVar
+
+# 如果您使用 Python 3.12+
+type PushOperation[T] = tuple[Literal['push'], T]
+type PopOperation = tuple[Literal['pop']]
+type StackOperation[T] = PushOperation[T] | PopOperation
+
+def stack_operations[T](operations: list[StackOperation[T]]):
     """
     练习 5.1.1: 用列表实现堆栈
     
@@ -50,26 +82,45 @@ def stack_operations(operations):
         dict: 堆栈操作结果
     """
     # TODO: 实现堆栈操作
+
+    stack: list[T] = []
+    for operation in operations:
+        if operation[0] == 'push':
+            stack.append(operation[1])
+        elif operation[0] == 'pop':
+            stack.pop()
+    return {'final_stack': stack, 'popped_items': stack}
+
     # 返回 {'final_stack': list, 'popped_items': list}
-    pass
 
 
-def queue_operations(operations):
+type EnqueueOperation[T] = tuple[Literal['enqueue'], T]
+type DequeueOperation = tuple[Literal['dequeue']]
+type QueueOperation[T] = EnqueueOperation[T] | DequeueOperation
+
+def queue_operations[T](operations: list[QueueOperation[T]]):
     """
     练习 5.1.2: 用列表实现队列（使用 collections.deque）
     
     Args:
-        operations (list): 操作列表，格式：[('enqueue', value), ('dequeue',), ...]
+        operations: 操作列表，格式：[('enqueue', value), ('dequeue',), ...]
     
     Returns:
         dict: 队列操作结果
     """
+    queue: list[T] = []
+    for operation in operations:
+        if operation[0] == 'enqueue':
+            queue.append(operation[1])
+        elif operation[0] == 'dequeue':
+            queue.pop(0)
+    return {'final_queue': queue, 'dequeued_items': queue}
+
     # TODO: 使用 deque 实现队列操作
     # 返回 {'final_queue': list, 'dequeued_items': list}
-    pass
 
 
-def list_comprehensions_practice(numbers):
+def list_comprehensions_practice(numbers: list[int]):
     """
     练习 5.1.3: 列表推导式练习
     
@@ -85,10 +136,16 @@ def list_comprehensions_practice(numbers):
     # 'positive': 正数列表
     # 'squares_of_evens': 偶数的平方列表
     # 'abs_values': 绝对值列表
-    pass
+
+    squares = [x**2 for x in numbers]
+    evens = [x for x in numbers if x % 2 == 0]
+    positive = [x for x in numbers if not (x <= 0)]
+    squares_of_evens = [x**2 for x in numbers if x % 2 == 0]
+    abs_values = [abs(x) for x in numbers]
+    return {'squares': squares, 'evens': evens, 'positive': positive, 'squares_of_evens': squares_of_evens, 'abs_values': abs_values}
 
 
-def nested_list_comprehension(matrix):
+def nested_list_comprehension(matrix: list[list[int]]):
     """
     练习 5.1.4: 嵌套列表推导式
     
@@ -104,10 +161,15 @@ def nested_list_comprehension(matrix):
     # 'diagonal': 对角线元素（如果是方阵）
     # 'row_sums': 每行元素之和
     # 'col_sums': 每列元素之和
-    pass
+    transposed = [[row[i] for row in matrix] for i in range(len(matrix[0]))]
+    flattened = [item for sublist in matrix for item in sublist]
+    diagonal = [matrix[i][i] for i in range(len(matrix))]
+    row_sums = [sum(row) for row in matrix]
+    col_sums = [sum(matrix[i][j] for i in range(len(matrix))) for j in range(len(matrix[0]))]
+    return {'transposed': transposed, 'flattened': flattened, 'diagonal': diagonal, 'row_sums': row_sums, 'col_sums': col_sums}
 
 
-def tuple_operations(data_list):
+def tuple_operations(data_list: list[int]):
     """
     练习 5.3: 元组和序列操作
     
@@ -123,10 +185,16 @@ def tuple_operations(data_list):
     # 'nested_tuple': 嵌套元组 ((1, 2), (3, 4))
     # 'tuple_concatenation': 两个元组连接的结果
     # 'tuple_repetition': 元组重复3次的结果
-    pass
+
+    tuple_created = tuple(data_list)
+    unpacked = tuple_created[0], tuple_created[1], tuple_created[2]
+    nested_tuple = ((1, 2), (3, 4))
+    tuple_concatenation = tuple_created + nested_tuple
+    tuple_repetition = tuple_created * 3
+    return {'tuple_created': tuple_created, 'unpacked': unpacked, 'nested_tuple': nested_tuple, 'tuple_concatenation': tuple_concatenation, 'tuple_repetition': tuple_repetition}
 
 
-def set_operations(set1, set2):
+def set_operations(set1: set[int], set2: set[int]):
     """
     练习 5.4: 集合操作
     
@@ -145,10 +213,17 @@ def set_operations(set1, set2):
     # 'is_subset': set1 是否是 set2 的子集
     # 'is_superset': set1 是否是 set2 的超集
     # 'is_disjoint': 两个集合是否无交集
-    pass
+    union = set1 | set2
+    intersection = set1 & set2
+    difference = set1 - set2
+    symmetric_difference = set1 ^ set2
+    is_subset = set1 <= set2
+    is_superset = set1 >= set2
+    is_disjoint = set1.isdisjoint(set2)
+    return {'union': union, 'intersection': intersection, 'difference': difference, 'symmetric_difference': symmetric_difference, 'is_subset': is_subset, 'is_superset': is_superset, 'is_disjoint': is_disjoint}
 
 
-def dictionary_operations(data):
+def dictionary_operations(data: list[tuple[str, int]]):
     """
     练习 5.5: 字典操作
     
@@ -164,10 +239,23 @@ def dictionary_operations(data):
     # 'values_list': 所有值的列表
     # 'items_list': 所有键值对的列表
     # 'dict_comprehension': 使用字典推导式创建 {key: value*2} 的字典
-    pass
+    created_dict = dict(data)
+    keys_list = list(created_dict.keys())
+    values_list = list(created_dict.values())
+    items_list = list(created_dict.items())
+    dict_comprehension = {key: value * 2 for key, value in created_dict.items()}
+    return {'created_dict': created_dict, 'keys_list': keys_list, 'values_list': values_list, 'items_list': items_list, 'dict_comprehension': dict_comprehension}
 
+class StudentData(TypedDict, total=True):
+    """
+    学生数据结构，严格限定只包含以下字段：
+    - name: 学生姓名  
+    - scores: 学生分数列表
+    """
+    name: str
+    scores: list[int]
 
-def advanced_dictionary_practice(students_data):
+def advanced_dictionary_practice(students_data: list[StudentData]):
     """
     练习 5.5: 高级字典练习
     
@@ -182,10 +270,14 @@ def advanced_dictionary_practice(students_data):
     # 'top_student': 平均分最高的学生姓名
     # 'passing_students': 平均分>=60的学生列表
     # 'subject_averages': 如果每个学生有相同数量的科目，计算每科的平均分
-    pass
+    average_scores = {student['name']: sum(student['scores']) / len(student['scores']) for student in students_data}
+    top_student = max(average_scores.items(), key=lambda x: x[1])[0]
+    passing_students = [student for student in students_data if sum(student['scores']) / len(student['scores']) >= 60]
+    subject_averages = {i: sum(student['scores'][i] for student in students_data) / len(students_data) for i in range(len(students_data[0]['scores']))}
+    return {'average_scores': average_scores, 'top_student': top_student, 'passing_students': passing_students, 'subject_averages': subject_averages}
 
 
-def looping_techniques_demo(data_dict):
+def looping_techniques_demo(data_dict: dict[str, int]):
     """
     练习 5.6: 循环的技巧
     
@@ -202,10 +294,16 @@ def looping_techniques_demo(data_dict):
     # 'reversed_items': 反向循环的结果
     # 'sorted_by_key': 按键排序循环的结果
     # 'sorted_by_value': 按值排序循环的结果
-    pass
+    items_loop = list(data_dict.items())
+    enumerate_keys = list(enumerate(data_dict.keys()))
+    zip_keys_values = list(zip(data_dict.keys(), data_dict.values()))
+    reversed_items = list(reversed(data_dict.items()))
+    sorted_by_key = sorted(data_dict.items(), key=lambda x: x[0])
+    sorted_by_value = sorted(data_dict.items(), key=lambda x: x[1])
+    return {'items_loop': items_loop, 'enumerate_keys': enumerate_keys, 'zip_keys_values': zip_keys_values, 'reversed_items': reversed_items, 'sorted_by_key': sorted_by_key, 'sorted_by_value': sorted_by_value}
 
 
-def sequence_comparison(seq1, seq2):
+def sequence_comparison(seq1: list[int], seq2: list[int]):
     """
     练习 5.8: 序列和其他类型的比较
     
@@ -218,11 +316,18 @@ def sequence_comparison(seq1, seq2):
     """
     # TODO: 返回包含以下比较结果的字典：
     # 'equal': 是否相等
+    equal = all(x == y for x, y in zip(seq1, seq2))
     # 'lexicographic': 字典序比较结果 (-1, 0, 1)
+    lexicographic = 0 if seq1 == seq2 else -1 if seq1 < seq2 else 1
     # 'length_comparison': 长度比较结果
+    length_comparison = -1 if len(seq1) < len(seq2) else 1 if len(seq1) > len(seq2) else 0
     # 'type_same': 类型是否相同
+    type_same = type(seq1) == type(seq2)
     # 'first_difference_index': 第一个不同元素的索引（如果存在）
-    pass
+    first_difference_index = next(
+        (i for i, (x, y) in enumerate(zip(seq1, seq2)) if x != y), -1
+    )
+    return {'equal': equal, 'lexicographic': lexicographic, 'length_comparison': length_comparison, 'type_same': type_same, 'first_difference_index': first_difference_index}
 
 
 def del_statement_demo(original_list):
@@ -240,7 +345,15 @@ def del_statement_demo(original_list):
     # 'del_slice': 删除切片 [1:3] 后的列表
     # 'del_every_second': 删除每隔一个元素后的列表
     # 'clear_list': 清空列表后的结果
-    pass
+    del_by_index = original_list.copy()
+    del del_by_index[2]
+    del_slice = original_list.copy()
+    del del_slice[1:3]
+    del_every_second = original_list.copy()
+    del del_every_second[::2]
+    clear_list = original_list.copy()
+    clear_list.clear()
+    return {'del_by_index': del_by_index, 'del_slice': del_slice, 'del_every_second': del_every_second, 'clear_list': clear_list}
 
 
 if __name__ == "__main__":
